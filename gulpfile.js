72911c1306eeca194fdc61a58fd973cbe2c87828
PavelGQ <pavelgq@yandex.ru>
2020-03-16 23:07:37 +0300

    Добавил блок с описанием и примером

--- gulpfile.js
+++ gulpfile.js
@@ -22,24 +22,24 @@ var csscomb = require('csscomb');
 var del = require("del");
 
 //Создание файла стилей и минификация
-gulp.task("style", function(callback){
+gulp.task("style", function (callback) {
   gulp.src("less/style.less")
-  .pipe(plumber())
-  .pipe(less())
-  .pipe(postcss([
-    autoprefixer()
-  ]))
-  .pipe(gulp.dest("build/css"))
-  .pipe(minify())
-  .pipe(rename("style.min.css"))
-  .pipe(gulp.dest("build/css"))
-  .pipe(server.stream());
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
 
   callback();
 });
 
 // Минификация js
-gulp.task("uglify", function() {
+gulp.task("uglify", function () {
   return gulp.src("js/*.js")
     .pipe(uglify())
     .pipe(rename({
@@ -62,40 +62,53 @@ gulp.task("html", function () {
 // Формирование изображений в формате webp
 gulp.task("webp", function () {
   return gulp.src("img/**/*.{png,jpg}")
-    .pipe(webp({quality: 90}))
+    .pipe(webp({
+      quality: 90
+    }))
     .pipe(gulp.dest("build/img"));
 });
 
 // Создание векторного спрайта
 gulp.task("sprite", function () {
-  return gulp.src("img/icon-*.svg")
+  return gulp.src("img/svg/icon-*.svg")
     .pipe(svgSprite({
+      selector: "i-sp-%f",
+      svg: {
+        sprite: "svg.svg"
+      },
+      svgPath: "%f",
+      cssFile: "svg_sprite.css",
+      common: "ic",
       inlineSvg: true
     }))
     .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("build/img"));
+    .pipe(gulp.dest("build/img/svg"));
 });
 
 // Сортировка css-свойств
-gulp.task("csscomb", function() {
+gulp.task("csscomb", function () {
   return gulp.src("less/blocks/*.less")
     .pipe(csscomb())
     .pipe(gulp.dest("sass/blocks/"));
 });
 
 // Оптимизация изображений
-gulp.task("imagemin", function() {
+gulp.task("imagemin", function () {
   return gulp.src("img/**/*.{png,jpg,svg}")
     .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng({
+        optimizationLevel: 3
+      }),
+      imagemin.jpegtran({
+        progressive: true
+      }),
       imagemin.svgo()
     ]))
     .pipe(gulp.dest("img"));
 });
 
 // Запуск сервера со слежением за необходимыми файлами
-gulp.task("serve", function() {
+gulp.task("serve", function () {
   server.init({
     server: "build/",
     notify: false,
@@ -110,32 +123,31 @@ gulp.task("serve", function() {
 
 });
 // Копирование файлов
-gulp.task("copy", function() {
+gulp.task("copy", function () {
   return gulp.src([
-    "fonts/*.{woff,woff2}",
-    "img/**",
-    "js/*.js"
-  ], {
-    base: "."
-  })
-  .pipe(gulp.dest("build"));
+      "fonts/*.{woff,woff2}",
+      "img/**",
+      "js/*.js"
+    ], {
+      base: "."
+    })
+    .pipe(gulp.dest("build"));
 });
 
 
 // Удаление файлов
-gulp.task("clean", function() {
+gulp.task("clean", function () {
   return del("build");
 });
 
 
 // Сборка проекта
 gulp.task("build", gulp.series(
-    "clean",
-    "copy",
-    "style",
-    "uglify",
-    "sprite",
-    "html",
-    "webp"
-  )
-  );
+  "clean",
+  "copy",
+  "style",
+  "uglify",
+  "sprite",
+  "html",
+  "webp"
+));
