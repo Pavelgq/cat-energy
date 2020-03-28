001782f04e404388d28b952b600b2f464034a707
PavelGQ <pavelgq@yandex.ru>
2020-03-28 23:00:50 +0300

    Прописал открытие меню в мобильной версии

---
+++ js/action.js
@@ -0,0 +1,38 @@
+'use struct'
+
+
+var nav = document.querySelector(".main-nav");
+var burger = nav.querySelector(".main-nav__toggle");
+var viewportWidth = document.body.clientWidth;
+
+var decktopWidth = 1440;
+var tabletWidth = 768;
+
+//nojs
+nav.classList.remove('main-nav--nojs');
+
+//Menu mobile version
+function burgerClickMenu(){
+  nav.classList.toggle("main-nav--closed");
+  nav.classList.toggle("main-nav--opened");
+
+}
+
+burger.addEventListener('click', burgerClickMenu);
+
+
+//experience block
+
+var exp = document.querySelector(".experience__interactive");
+var before = exp.querySelector(".experience__button--before");
+var after = exp.querySelector(".experience__button--after");
+var rabbitBefore = exp.querySelector(".experience__rabbit--before");
+var rabbitAfter = exp.querySelector(".experience__rabbit--after");
+
+function changeCatProgress () {
+  rabbitBefore.classList.toggle('experience__rabbit--show');
+  rabbitAfter.classList.toggle('experience__rabbit--show');
+}
+
+before.addEventListener('click', changeCatProgress);
+after.addEventListener('click', changeCatProgress);
